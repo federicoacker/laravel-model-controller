704f04c8d21a8a894ae084aaee5d043bdc61aa98
federicoacker <acker.federico@gmail.com>
2026-09-15 17:07:54 +0200

    Progetto inizializzato con il model Movie fatto

---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="it">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Laravel Model Controller</title>
+    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+</head>
+<body>
+    <main>
+
+    </main>
+</body>
+</html>
